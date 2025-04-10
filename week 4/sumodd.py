ffe7f3f8aaeaeddd3771b10280f676a6e62b0fdc
vallari114 <vallariperumandla05@gmail.com>
2025-04-10 12:07:50 +0530

    Create sumodd.py

---
+++ week 4/sumodd.py
@@ -0,0 +1,13 @@
+def evenindexsum(l):
+    evensum=0
+    for i in range(len(i)):
+           if i % 2 ==0:
+                   evensum+=l[i]
+    return evensum
+def oddindexsum(l1):
+    oddsum=0
+    for i in range(len(i)):
+          if i % 1 ==0:
+                 oddsum+=l1[i]
+    return oddsum
+    
